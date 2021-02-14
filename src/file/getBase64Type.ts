86ceadcc790e421f8a9183001f63b6084e307fca
usama <usamaasfar.mua@gmail.com>
2021-02-14 23:14:36 +0530

    fix: base64Type -> getBase64Type

--- src/file/getBase64Type.ts
+++ src/file/getBase64Type.ts
@@ -1,3 +1,3 @@
-export default function base64Type(base64: string) {
+export default function getBase64Type(base64: string) {
   return base64.substring('data:'.length, base64.indexOf(';base64'))
 }
